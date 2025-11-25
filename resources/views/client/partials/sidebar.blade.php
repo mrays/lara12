6c63df2b97090f048ada4405da2c8e945713970f
arif saputra <ArifYogiSaputra_1701620136@mhs.unj.ac.id>
2025-11-25 10:26:32 +0700

    sa

--- resources/views/client/partials/sidebar.blade.php
+++ resources/views/client/partials/sidebar.blade.php
@@ -1,32 +1,48 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand">
-        <a href="/admin" class="app-brand-link">
-            <span class="app-brand-text h5">Admin</span>
+        <a href="{{ route('client.dashboard') }}" class="app-brand-link">
+            <span class="app-brand-text h5">Client Portal</span>
         </a>
     </div>
 
     <ul class="menu-inner py-1">
-        <li class="menu-item">
-            <a href="/admin" class="menu-link">
+        <!-- Dashboard -->
+        <li class="menu-item {{ request()->routeIs('client.dashboard') ? 'active' : '' }}">
+            <a href="{{ route('client.dashboard') }}" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
                 <span>Dashboard</span>
             </a>
         </li>
 
-        <li class="menu-item">
-            <a href="/admin/clients" class="menu-link">
-                <span>Clients</span>
+        <!-- My Services -->
+        <li class="menu-item {{ request()->routeIs('client.services.*') ? 'active' : '' }}">
+            <a href="/client/services" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-package"></i>
+                <span>My Services</span>
             </a>
         </li>
 
-        <li class="menu-item">
-            <a href="/admin/services" class="menu-link">
-                <span>Services</span>
-            </a>
-        </li>
-
-        <li class="menu-item">
-            <a href="/admin/invoices" class="menu-link">
+        <!-- Invoices -->
+        <li class="menu-item {{ request()->routeIs('client.invoices.*') ? 'active' : '' }}">
+            <a href="{{ route('client.invoices.index') }}" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-receipt"></i>
                 <span>Invoices</span>
+            </a>
+        </li>
+
+        <!-- Upgrade Requests -->
+        <li class="menu-item {{ request()->routeIs('client.upgrade-requests.*') ? 'active' : '' }}">
+            <a href="{{ route('client.upgrade-requests.index') }}" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-up-arrow-alt"></i>
+                <span>Upgrade Requests</span>
+            </a>
+        </li>
+
+        <!-- Profile -->
+        <li class="menu-item {{ request()->routeIs('profile.*') ? 'active' : '' }}">
+            <a href="{{ route('profile.edit') }}" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-user"></i>
+                <span>Profile</span>
             </a>
         </li>
     </ul>
