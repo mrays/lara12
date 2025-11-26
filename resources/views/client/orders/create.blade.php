175a3a57ab19aa24ffd5ccc75ef0c3699abc6db8
arif saputra <ArifYogiSaputra_1701620136@mhs.unj.ac.id>
2025-11-26 19:10:34 +0700

    sasa

--- resources/views/client/orders/create.blade.php
+++ resources/views/client/orders/create.blade.php
@@ -247,7 +247,7 @@
                                         <input class="form-check-input" type="radio" name="billing_cycle" 
                                                id="billingAnnual" value="annually" checked>
                                         <label class="form-check-label" for="billingAnnual">
-                                            {{ \App\Http\Controllers\ServiceManagementController::staticTranslateBillingCycle('annually') }}
+                                            Tahunan
                                         </label>
                                     </div>
                                 </div>
